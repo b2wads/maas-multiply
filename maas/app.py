47f7453479a2d752832511e6ce7f7fbe9b5a45c4
Luiz Matos <luiz.matos@b2wdigital.com>
2019-06-18 16:27:33 -0300

    Implementado multiplicação.

--- maas/app.py
+++ maas/app.py
@@ -6,4 +6,7 @@ app = App("", "", "", 1)
 
 @app.route(["/"], type=RouteTypes.HTTP, methods=["POST"])
 async def operation(request: web.Request):
-    return web.json_response({})
+    data = await request.json()
+    left = data['left']
+    right = data['right']
+    return web.json_response({"result": left*right})
---
+++ tests/test_operation_multiply.py
@@ -0,0 +1,18 @@
+from asynctest import skip
+
+from maas.app import app
+from tests.base import BaseApiTestCase
+
+
+class OperationMultiplyTest(BaseApiTestCase):
+    async def setUp(self):
+        self.client = await self.aiohttp_client(app)
+
+    async def tearDown(self):
+        await super(OperationMultiplyTest, self).tearDown()
+
+    async def test_operation_endpoint(self):
+        result = await self.client.post("/", json={"left": 10, "right": 10})
+        self.assertEqual(200, result.status)
+        data = await result.json()
+        self.assertEqual({'result': 100}, data)
